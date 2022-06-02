59c1a7e59f07ace781fd84c5592a7bb219c709a9
Christian Anderson <christian.p.anderson1@gmail.com>
2022-06-01 23:05:33 -0400

    set SDK to 17

--- customer/src/main/java/com/christian/customer/Customer.java
+++ customer/src/main/java/com/christian/customer/Customer.java
@@ -5,13 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.GenerationType;
 
 @Data
 @Builder
-@Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Customer {
 
     @Id

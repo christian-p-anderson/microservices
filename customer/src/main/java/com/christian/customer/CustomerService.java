46b30c1acddb219b9bd7ad90432f8bad7fce2fcd
Christian Anderson <christian.p.anderson1@gmail.com>
2022-06-10 19:33:47 -0400

    check if customer is a fraud

--- customer/src/main/java/com/christian/customer/CustomerService.java
+++ customer/src/main/java/com/christian/customer/CustomerService.java
@@ -2,11 +2,13 @@ package com.christian.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -16,8 +18,17 @@ public class CustomerService {
                 .build();
         // todo: check if email valid
         // todo: check if email not taken
+        customerRepository.saveAndFlush(customer);
         // todo: check if fraudster
-        customerRepository.save(customer);
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http:localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+        assert fraudCheckResponse != null;
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
         // todo: send notification
     }
 }

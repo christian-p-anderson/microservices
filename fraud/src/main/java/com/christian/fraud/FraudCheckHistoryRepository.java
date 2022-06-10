2e6c0781e4dae5c5ad7b306c5c67d5dbcb42a63e
Christian Anderson <christian.p.anderson1@gmail.com>
2022-06-09 22:02:42 -0400

    created Fraud Check History repository

---
+++ fraud/src/main/java/com/christian/fraud/FraudCheckHistoryRepository.java
@@ -0,0 +1,6 @@
+package com.christian.fraud;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FraudCheckHistoryRepository extends JpaRepository<FraudCheckHistory, Integer> {
+}
